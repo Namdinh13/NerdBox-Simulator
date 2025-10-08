48bd5b9468d902dff8cfb3d1e0dbc30c27fad475
Namdinh13 <abct53085@gmail.com>
2025-10-08 17:26:07 +0700

    fix sitting chair

---
+++ Assets/MyGame/Scripts/Chair/Chair.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Chair : MonoBehaviour, IInteractable
+{
+    public Transform seatPoint, standPoint;
+    public GameObject player;
+    public float sitYOffset = 0.35f;
+
+    bool sitting;
+    Collider playerCol;
+    Collider chairCol;
+
+    void Start()
+    {
+        playerCol = player.GetComponentInChildren<Collider>();
+        chairCol = GetComponent<Collider>();
+    }
+
+    public void Interact()
+    {
+        if (!sitting)
+        {
+            Physics.IgnoreCollision(playerCol, chairCol, true);
+
+            player.transform.position = seatPoint.position - new Vector3(0, sitYOffset, 0);
+            Vector3 euler = seatPoint.rotation.eulerAngles;
+            player.transform.rotation = Quaternion.Euler(0, euler.y, 0);
+
+            sitting = true;
+        }
+        else
+        {
+            Physics.IgnoreCollision(playerCol, chairCol, false);
+            player.transform.position = standPoint.position;
+
+            sitting = false;
+        }
+    }
+}
+
